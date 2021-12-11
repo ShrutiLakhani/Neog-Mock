c6717f59c44cd901bef5a1890ea94eb5d914f9fd
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2021-12-11 20:11:50 +0530

    Changes made for adding color

---
+++ script.js
@@ -0,0 +1,15 @@
+var inputText = document.querySelector("#text-inp");
+var outputVal = document.querySelector("#output");
+var btnRed = document.querySelector("#btn-red");
+var btnBlue = document.querySelector("#btn-blue");
+
+btnRed.addEventListener("click", redText)
+//btnBlue.addEventListener("click", blueText)
+
+function redText()
+{
+    textInput = inputText.value.toUpperCase();
+    console.log(textInput)
+    outputVal.textContent = textInput;
+    outputVal.style.color = "red"
+}
